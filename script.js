d1ef85a41c8397ecb37bea1c3ae755455ae1bcf2
Filipe Pio Magalhães <filipepio2005@hotmail.com>
2024-01-26 11:40:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let acc = document.querySelectorAll(".doubts .content .content_right .accordion button span");
+
+  acc.forEach(item => {
+    item.addEventListener("click", () => {
+      // Alternar a classe "active" no botão pai
+      let button = item.parentElement;
+      button.classList.toggle("active");
+  
+      let panel = button.nextElementSibling;
+  
+      if (button.classList.contains("active")) {
+        // Se o botão está ativo, exibe o painel
+        panel.style.display = "block";
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } else {
+        // Se o botão não está ativo, esconde o painel
+        panel.style.maxHeight = 0;
+        panel.addEventListener("transitionend", () => {
+          panel.style.display = "none";
+        }, { once: true });
+      }
+    });
+  });
+});
+
+/*
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const telephone = document.getElementById('telephone')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkInputs()
+})
+
+function checkInputs() {
+
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const telephoneValue = telephone.value.trim()
+
+    if(usernameValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(username, 'Preencha esse campo')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(username)
+    }
+
+    if(emailValue === '') {
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
+    } else {
+        setSuccessFor(email)
+    }
+   
+
+    if (telephoneValue === '') {
+      setErrorFor(telephone, 'Preencha esse campo');
+    } else if (telephoneValue.length < 11) {
+      setErrorFor(telephone, 'Digite o número do telefone com DDD');
+    } else if (telephoneValue.length > 11) {
+      setErrorFor(telephone, 'Mais de 11 números, apenas DDD e o número!');
+    } else {
+      setSuccessFor(telephone);
+    }
+
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
+*/
